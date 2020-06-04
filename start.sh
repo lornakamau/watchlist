5f4bc088395b32c94090916379be44706c40f756
Lorna Kamau <kamaulorna@gmail.com>
2020-06-04 12:18:36 +0300

    initial commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='995c639d4d76b60467f25347e9f43f00'
+export SECRET_KEY='<Flask WTF Secret Key>'
+export MAIL_USERNAME='lornadeveloper@gmail.com'
+export MAIL_PASSWORD='lwkamaum'
+python3.6 manage.py server
